c1f559b1e04a5d1843afcfb1ffe9817db949e42f
Ayush Kumar <ayushtiwari0803@gmail.com>
2025-04-17 01:32:07 +0530

    userAuth middleware added

---
+++ Backend/src/middleware/auth.js
@@ -0,0 +1,30 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const User = require("../models/users");
+
+const jwtSecret = process.env.JWT_SECRET;
+
+const userAuth = async (req, res, next) =>{
+    try {
+        const {token} = res.cookies;
+        if (!token){
+            res.status(401).send("Unauthorized");
+        }
+
+        const verifyUser = await jwt.verify(token, jwtSecret)
+        const {_id} = verifyUser;
+
+        const user = await User.findById(_id);
+        if(!user){
+            res.status(404).send("User doesn't exists");
+        }
+        req.user = user;
+        next();
+    } catch (error) {
+        res.status(400).send("Error:"+ error.message)
+    }
+}
+
+module.exports = {
+    userAuth
+}
