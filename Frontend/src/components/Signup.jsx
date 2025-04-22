98528726f055dcaa84b5b3cdf5c067ab533b708b
Ayush Kumar <ayushtiwari0803@gmail.com>
2025-04-22 23:12:50 +0530

    Added backend url to frontend

--- Frontend/src/components/Signup.jsx
+++ Frontend/src/components/Signup.jsx
@@ -16,7 +16,7 @@ const Signup = () => {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post('http://localhost:3001/login', {
+      const res = await axios.post(`${process.env.BASE_URL}+login`, {
         email,
         password
       }, {
@@ -32,7 +32,7 @@ const Signup = () => {
   const handleSignup = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post('http://localhost:3001/signup', {
+      const res = await axios.post(`${process.env.BASE_URL}+signup`, {
         name,
         email,
         password
